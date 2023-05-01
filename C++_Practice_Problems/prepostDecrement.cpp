94998c65951acffc68a8e853bda237f081c509f7
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-05-01 12:56:32 +0530

    Pre Post Decrement

---
+++ C++_Practice_Problems/prepostDecrement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    a += 2; /*a = a + 2*/
+    a *= 3; /*a = a * 3*/
+
+
+    cout << a-- << endl;
+
+    cout << --a << endl;
+     
+}
