83a1e901295277b7ceb69c432bb139833b0b52b3
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-08 15:59:16 +0530

    To find the largest element in Array

---
+++ StriverDSAsheet/Arrays/largest_element.cpp
@@ -0,0 +1,23 @@
+//optimal solution
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int largest = arr[0];
+    for(int i = 0; i < n; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        } 
+    }
+    cout << largest << endl;
+    return 0;
+}
+//TC --> O(N)
