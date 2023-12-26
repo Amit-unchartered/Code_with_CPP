ef93c60c61b6744276bca689600c56065e9d79c0
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-26 12:39:48 +0530

    modified

--- Codeforces/1152A.cpp
+++ Codeforces/1152A.cpp
@@ -1,32 +1,39 @@
 #include<bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 int main()
 {
-    int n; int m;
+    ll n; ll m;
     cin >> n; cin >> m;
-    int a[n];
-    for(int i = 0; i < n; i++){
+    ll a[n];
+    for(ll i = 0; i < n; i++){
     cin >> a[i];
     }
     
-    int b[m];
-    for(int i = 0; i < m; i++){
+    ll b[m];
+    for(ll i = 0; i < m; i++){
     cin >> b[i];
     }
-
-    int cnt = 0;
-    for(int i=0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if((a[i]+b[j])%2 == 1){
-                i++;
-                j++;
-                if(j > m-1 || i > n-1){
-                break;
-            }
-                cnt++;
-            }
+    
+    int odd1=0; int even1=0;
+    int odd2=0; int even2=0;
+    ll cnt = 0;
+    for(ll i=0; i < n; i++) {
+        if(a[i]%2 == 0){
+            even1++;
+        }else if(a[i]%2 == 1){
+            odd1++;
         }
     }
-    cout << cnt << endl;
-    return 0;
+    
+    for(ll i=0; i < m; i++) {
+        if(b[i]%2 == 0){
+            even2++;
+        }else if(b[i]%2 == 1){
+            odd2++;
+        }
+    }
+    
+    cout << min(even1,odd2)+min(odd1,even2) << endl;
 }
+    
