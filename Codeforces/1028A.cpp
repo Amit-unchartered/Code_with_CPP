cc3ecb0e295bc63a27021bd2bc82b79076b61641
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-27 22:35:13 +0530

    1028A

--- Codeforces/1028A.cpp
+++ Codeforces/1028A.cpp
@@ -1,15 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 2e5+5;
+char a[1005][1005];
+
 int main()
 {
-    int n; cin>>n;
-    int m; cin>>m;
-    string s;
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> s; 
+    int n;int m; 
+    cin >> n >>m;
+    int x1=0, y1 =0, x2=0, y2=0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cin >> a[i][j];
+            if(a[i][j] == 'B' && x1 == 0){
+                x1 = i; y1 = j;
+            }
+            if(a[i][j] == 'B') x2=i; y2 = j; 
         }
     }
+
+    cout << (x1+x2)/2 << " " << (y1+y2)/2 << endl;
     return 0;
 }
