49ad9de73a761bd8ea82b05f353264b6d7ebc605
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-28 22:50:25 +0530

    1915D

---
+++ Codeforces/1915D.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+
+    for(int i=0; i<s.length(); i++) {
+        
+    }
+    return 0;
+}
