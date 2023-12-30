1b3f8c57474e84955d5dbf3bb79d3b915cb397ad
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2023-12-30 15:00:38 +0530

    optimal_zero_&_positive

---
+++ StriverDSAsheet/Arrays/longest_subarray_sumk.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int left = 0, right = 0;
+    long long sum = a[0];
+    int maxlen = 0;
+    int n = a.size();
+
+    while(right < n){
+        while(left<=right && sum > k){
+            sum -= a[left];
+            left++;
+        }
+
+        if(sum == k){
+            maxlen = max(maxlen, right-left+1);
+        }
+
+        right++;
+        if(right < n) sum += a[right];
+    }
+}
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    return 0;
+}
