5ff7874385814b5c30e0efcf5f1261505c75ab99
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-02-03 20:34:08 +0530

    Number Theory

---
+++ codencode/Number_theory.cpp
@@ -0,0 +1,8 @@
+//PART 1
+//primality test, //prime factorization, //sieve of eratosthenes, //binary exponentiation, //euclid algorithm forGCD
+//goldbatch's conjecture, //finding number of divisors of N, //calculating binomial coefficient, //segmented sieve, //modulo inverse
+
+//PART 2
+// extended euclidian algorithm, //linear diophantine equation, //matrix exponentiation, //fibonacci numbers in O(logn)
+//chinese remnainder theorem, //Euler Totient Function, //sum of divisors, //pollard p-1 method, //pollard rho algorithm
+
