2cdcbb1562f55ca8788e39bfbc84ddad20a7d5b5
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-02-03 20:33:50 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ff first
+#define ss second
+#define ii pair<int, int>
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+
+int main(){
+    
+}
