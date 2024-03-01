c824fbc090a2242c830c7bf26e72bc5d224b2c12
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-03-01 23:56:08 +0530

    check if given element is present in LL

---
+++ StriverDSAsheet/linked_list/check_if_present.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        Node* tmp = new Node(arr[i]);
+        mover->next = tmp;
+        mover = mover->next; // mover = tmp;
+    }
+    return head;
+}
+
+int checkIfPresent(Node* head, int val){
+    Node* tmp = head;
+    while(tmp){
+        if(tmp->data == val) return 1;
+        tmp = tmp->next;
+    }
+    return 0;
+}
+
+int main()
+{
+    vector<int> arr = {12, 5, 8, 6};
+    Node* head = convertArr2LL(arr);
+    
+    cout << checkIfPresent(head, 8);
+    return 0;
+}
