f89baa85c6897df2f26ab5b13ec785afce53b69f
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-03-05 03:59:07 +0530

    Enumeration and Friend Function

---
+++ OOPS/enum_friend_function.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+enum direction{North, South, East, West};
+
+int main()
+{
+    direction here;
+    here = South;
+    cout << "Direction: " << here+1;
+    return 0;
+}
