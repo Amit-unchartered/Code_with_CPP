b675240a96aa13342834146f870c0b7687687cf7
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-03-16 18:01:46 +0530

    modified

--- StriverDSAsheet/linked_list/DLL.cpp
+++ StriverDSAsheet/linked_list/DLL.cpp
@@ -40,10 +40,141 @@ void print(Node* head){
     }
 }
 
+Node* deleteHead(Node* head){
+    if((head == NULL) || (head->next == NULL)){
+        return NULL;
+    }
+    Node* prev = head;
+    head = head->next;
+    head->back = nullptr;
+    prev->next = nullptr;
+
+    delete prev; //free the heap memory
+    return head;
+}
+
+Node* deleteTail(Node* head){
+    if((head == NULL) || (head->next == NULL)){
+        return head;
+    }
+    Node* tail = head;
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+    Node* prev = tail->back;
+    tail->back = nullptr;
+    prev->next = nullptr;
+    delete tail;
+    return head;
+}
+
+Node* removeKthelement(Node* head, int k){
+    Node* tmp = head;
+    int cnt = 0; 
+    while(tmp != NULL){
+        cnt++;
+        if(cnt == k) break;
+        tmp = tmp->next;
+    }
+    Node* prev = tmp->back;
+    Node* front = tmp->next;
+    
+    //single element
+    if((prev == NULL) && (front == NULL)){
+        //delete head;
+        return NULL;
+    }
+    if(prev == NULL){
+        return deleteHead(head);
+    }
+    if(front == NULL){
+        return deleteTail(head);
+    }
+
+    prev->next = front;
+    front->back = prev;
+    tmp->next = nullptr;
+    tmp->back = nullptr;
+    delete tmp;
+    return head;
+}
+
+//We need to delete a node in this ques, it is given that the node can't be head
+void deleteNode(Node *temp){
+    Node* prev = temp->back;
+    Node* front = temp->next;
+
+    if(front == NULL){
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp);
+        return;
+    }
+
+    prev->next = front;
+    front->back = prev;
+    temp->next = nullptr;
+    temp->back = nullptr;
+    free(temp);
+    return;
+}
+
+//Insertion(before wala case)
+Node* insertBeforehead(Node* head, int val){
+    Node* newNode = new Node(val, head, nullptr);
+    head->back = newNode;
+    return newNode;
+}
+
+Node* insertBeforetail(Node* head, int val){
+    Node* tail = head;
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+    Node* prev = tail->back;
+    Node* newNode = new Node(val, tail, prev);
+    prev->next = newNode;
+    tail->back = newNode;
+    return head;
+}
+
+Node* insertBeforeKthelement(Node* head, int val, int k){
+    if(k == 1){
+        return insertBeforehead(head, val);
+    }
+    Node* tmp = head;
+    int cnt = 0;
+    while(tmp != NULL){
+        cnt++;
+        if(cnt == k) break;
+        tmp = tmp->next;
+    }
+    Node* prev = tmp->back;
+    Node* newNode = new Node(val, tmp, prev);
+    prev->next = newNode;
+    tmp->back = newNode;
+    return head;
+}
+
+void insertBeforeNode(Node* node, int val){
+    Node* prev = node->back;
+    Node* newNode = new Node(val, node, prev);
+    prev->next = newNode;
+    node->back = newNode;
+}
+
 int main()
 {
     vector<int> arr = {12, 5, 8, 7};
     Node* head = convertArr2DLL(arr);
+    //head = deleteHead(head);
+    //head = deleteTail(head);
+    //head =removeKthelement(head, 1);
+    //deleteNode(head->next->next);
+    //head = insertBeforehead(head, 10);
+    //head = insertBeforetail(head, 15);
+    //head = insertBeforeKthelement(head, 20, 4);
+    insertBeforeNode(head->next->next, 100);
     print(head);
     return 0;
 }
--- StriverDSAsheet/linked_list/DLinkedList.java
+++ StriverDSAsheet/linked_list/DLinkedList.java
@@ -36,11 +36,40 @@ public class DLinkedList {
         }
         return head; 
     }
+
+    private static Node deleteHead(Node head){
+        if((head == null) || (head.next == null)){
+            return null;
+        }
+        Node prev = head;
+        head = head.next;
+        head.back = null;
+        prev.next = null;
+    
+        //delete prev; //free the heap memory
+        return head;
+    }
+
+    private static Node deleteTail(Node head){
+        if((head == null) || (head.next == null)){
+            return head;
+        }
+        Node tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+        }
+        Node prev = tail.back;
+        tail.back = null;
+        prev.next = null;
+        //delete tail;
+        return head;
+    }
     public static void main(String[] args) {
         int[] arr = {12, 5, 6, 8};
         Node head = convertArr2DLL(arr);
         print(head);
+        //head = deleteHead(head);
+        head = deleteTail(head);
+        print(head);
     }
-
-  
 }
