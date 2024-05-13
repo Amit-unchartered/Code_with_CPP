947632ddf0196f255c207fd51ca4bccc657776cd
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-05-13 12:10:53 +0530

    Modified

--- StriverDSAsheet/linked_list/sort_ll_0_1_2.cpp
+++ StriverDSAsheet/linked_list/sort_ll_0_1_2.cpp
@@ -29,6 +29,77 @@ Node* convertArr2LL(vector<int> &arr){
     }
 }
 
+Node* sort012(Node* head){
+    Node* tmp = head;
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+    while(tmp != NULL){
+        if(tmp->data == 0){
+            cnt0++;
+        } 
+        else if(tmp->data == 1){
+            cnt1++;
+        }
+        else {
+            cnt2++;
+        }
+        tmp = tmp->next;
+    }
+    tmp = head;
+
+    while(tmp != NULL){
+        if(cnt0) {
+            tmp->data = 0;
+            cnt0--;
+    }else if(cnt1){
+        tmp->data = 1;
+        cnt1--;
+    }else{
+        tmp->data = 2;
+        cnt2--;
+    }
+    tmp = tmp->next;
+    }
+    return head;
+}
+
+//
+Node* sort012_optimal(Node* head){
+    if(head == NULL || head->next == NULL) return head;
+
+    Node* zeroHead = new Node(-1);
+    Node* oneHead = new Node(-1);
+    Node* twoHead = new Node(-1);
+
+    Node* zero = zeroHead;
+    Node* one = oneHead;
+    Node* two = twoHead;
+
+    Node* tmp = head;
+    while(tmp){
+        if(tmp->data == 0){
+            zero->next = tmp;
+            zero = zero->next;
+        }else if(tmp->data == 1){
+            one->next = tmp;
+            one = one->next;
+        }else{
+            two->next = tmp;
+            two = two->next;
+        }
+        tmp = tmp->next;
+    }
+    zero->next = (oneHead->next)?(oneHead->next):(twoHead->next);
+    one->next = twoHead->next;
+    two->next = NULL;
+    Node* newHead = zeroHead->next;
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    return newHead;
+}
+
+
 void print(Node* head){
     while(head != NULL){
         cout << head->data << " ";
