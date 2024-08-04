36af61f918517499941fc0003730a34e547b7c9f
Amit Kumar Agrawal <amitkumaragrawal42@gmail.com>
2024-08-04 22:24:33 +0530

    iterative postorder using single stack added

--- StriverDSAsheet/trees/postorder.cpp
+++ StriverDSAsheet/trees/postorder.cpp
@@ -55,3 +55,45 @@ public:
         return postorder;
     }
 };
+
+//Iterative postorder traversal using single stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        TreeNode *curr = root;
+        vector<int>postorder;
+        stack<TreeNode*> st;
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }else{
+                TreeNode* tmp = st.top()->right;
+                if(tmp==NULL){
+                    tmp = st.top();
+                    st.pop();
+                    postorder.push_back(tmp->val);
+                    while(!st.empty() && tmp==st.top()->right){
+                        tmp=st.top();
+                        st.pop();
+                        postorder.push_back(tmp->val);
+                    }
+                }else{
+                    curr=tmp;
+                }
+            }
+        }
+        return postorder;
+    }
+};
